c940aba510f8eedc264dc565ea3849473c26692b
mattiange <75547604+mattiawebdesigner@users.noreply.github.com>
2023-01-20 15:22:24 +0100

    Update send_single_message.php

--- ESEMPI/send_single_message.php
+++ ESEMPI/send_single_message.php
@@ -4,4 +4,4 @@ include_once '../class/autoload.php';
 $contatto = new SpokiContatto("3348768832", "Nome test1", "Cognome Test1");
 
 $spoki = new Spoki("your uuid");
-$spoki->sendSingleMessage($contatto, "Messaggio di prova API", "https://app.spoki.it/api/messages/");
+$spoki->sendSingleMessage($contatto, "Test Message", "https://app.spoki.it/api/messages/");
